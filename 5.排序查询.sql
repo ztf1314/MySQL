fc8955380411980cd55f0d9e98a2d3f93f082e92
wmh <minhuiwon@163.com>
2020-02-19 11:51:23 +0800

    第一次提交

---
+++ 5.排序查询.sql
@@ -0,0 +1,12 @@
+--排序
+    --order by 字段
+    --asc从小到达排序 即升序
+    --desc 从大到小排序 即将序
+    --在排序过程中遇到相同值时会按照主键进行排序
+
+    --查询年龄到18～34之间的男性，按照年龄从小到达排序
+    select * from studuent where (age between 12 and 23) and gender = 1 order by age desc;
+
+
+    --order by 多个字段
+    select * from studuent where (age between 12 and 23) and gender = 1 order by age desc, gender esc, height desc; --在年龄相同的情况下按照性别按照升序的方式排序
---
+++ 6.聚合函数.sql
@@ -0,0 +1,69 @@
+--求总数
+    --count
+    --查询男性有多少人
+    select * from studuent where gender = 1;
+    select count(*) from studuent where gender = 1;
+    select count(*) as 男性人数 from studuent where gender = 1;
+
+--求最大值
+    --max
+    --查询最大年龄
+    select max(age) as 最大值 from studuent; 
+
+--最小值
+    --min 秋女性年龄最小值
+    select min(age) as 最小值 from studuent where gender = 2; 
+
+--求和
+    --sum
+    --求所有年龄总和
+    select sum(age) as 和 from studuent; 
+
+--求平均值
+    --avg
+    select sum(age)/count(*) as 均值 from studuent;
+    select avg(age) as 最大值 from studuent;
+
+--四射误入
+    --round(123.232221133, 4)
+    select round(avg(age), 2) as 四射误入 from studuent;
+
+    --经验：语言里面小数是由误差的，例如银行当中，但是整数没有
+    --举例子：3.14 我们乘以100变成整数。
+
+--分组
+    --group by
+    --按照性别分组，查询所有的性别
+    select gender from studuent group by gender;
+
+    --计算每种性别的人数
+    select gender, count(*) from studuent group by gender;  --count(*)表示对分组的计算个数
+
+
+    --按照性别分组
+    select gender, group_concat(name) from studuent group by gender;    
+
+    --计算男性的人数
+    select gender, count(*) from studuent where gender = 1 group by gender;
+        +--------+----------+
+        | gender | count(*) |
+        +--------+----------+
+        | 男     |        3 |
+        +--------+----------+
+        1 row in set (0.00 sec)
+
+    select gender, group_concat(name, age, id), count(*) from studuent where gender = 1 group by gender;
+        +--------+---------------------------------+----------+
+        | gender | group_concat(name, age, id)     | count(*) |
+        +--------+---------------------------------+----------+
+        | 男     | 小黄172,十一1516,十二1817       |        3 |
+        +--------+---------------------------------+----------+
+        1 row in set (0.00 sec)
+
+    select gender, group_concat(name, "_", age, " ", id), count(*) from studuent where gender = 1 group by gender;
+        +--------+---------------------------------------+----------+
+        | gender | group_concat(name, "_", age, " ", id) | count(*) |
+        +--------+---------------------------------------+----------+
+        | 男     | 小黄_17 2,十一_15 16,十二_18 17       |        3 |
+        +--------+---------------------------------------+----------+
+        1 row in set (0.00 sec)
